e3506211d7a950eb36cd59428d7cf4f8540a0036
caladavid <davidcalae@gmail.com>
2024-06-08 19:56:39 -0400

    Updated the 'About Me' section and resume; added SVG icons

--- components/About.jsx
+++ components/About.jsx
@@ -5,6 +5,8 @@ import JsSVG from "./svg/jsSVG"
 import PythonSvg from "./svg/PythonSVG"
 import ReactSVG from "./svg/ReactSVG"
 import TailwindSVG from "./svg/TailwindSVG"
+import MySQLSVG from "./svg/MySQLSVG"
+import LaravelSVG from "./svg/LaravelSVG"
 import Wrapper from "./Wrapper"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, } from 'swiper/modules';
@@ -19,6 +21,8 @@ const skills = [
   { icon: NextjsSVG, label: "Next.js" },
   { icon: TailwindSVG, label: "Tailwind CSS" },
   { icon: PythonSvg, label: "Python" },
+  { icon: MySQLSVG, label: "MySQL" },
+  { icon: LaravelSVG, label: "Laravel" },
 ];
 
 const About = () => {
@@ -29,7 +33,7 @@ const About = () => {
         <div className="flex flex-col min-[769px]:flex-row gap-12 md:pt-11 ">
           <div className="min-[769px]:w-1/2 lg:ml-16 lg:text-xl">
             <p className="mb-4">Hey there! I&rsquo;m David Cala, and I&rsquo;m not just a web developer – I&rsquo;m a passionate problem-solver who loves building things from scratch. My journey in web development has been fueled by a deep love for technology and a desire to make a positive impact.</p>
-            <p className="mb-8">Since my youth, I&rsquo;ve been captivated by the world of computers and technology. From immersing myself in video games to exploring the depths of software, my love for technology has been the driving force behind my path to becoming a web developer.</p>
+            <p className="mb-8">I fell in love with coding while working on a WordPress website. When I decided to completely overhaul the design, I realized I needed to learn CSS. That curiosity gradually led me to explore frontend development, and I discovered my passion for writing code. Now, with several real-world projects under my belt, I&rsquo;m eager to pursue a full-time career in software engineering.</p>
             <div>
               <p className="mb-4 text-lg">Here are a few technologies I&rsquo;ve been working with recently:</p>
               <ul className="flex flex-wrap gap-8 text-lg text-center">
@@ -43,7 +47,7 @@ const About = () => {
                   slidesPerView={4}
                   spaceBetween={5}
                   autoplay={{
-                    delay: 2500,
+                    delay: 2000,
                     disableOnInteraction: false,
                   }}
                   loop={true}
